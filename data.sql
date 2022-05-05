2858406be759466fd7586eb876c0f5d1c58de8ee
Raul Souza Muniz <raulmuniz08@gmail.com>
2022-05-04 22:03:03 -0300

    Novo commit_teste

---
+++ data.sql
@@ -0,0 +1,21 @@
+CREATE TABLE mytable(
+   Nome                    VARCHAR(10) NOT NULL PRIMARY KEY
+  ,Idade                   INTEGER  NOT NULL
+  ,AnosdeExperiencianaArea INTEGER  NOT NULL
+);
+INSERT INTO mytable(Nome,Idade,AnosdeExperiencianaArea) VALUES ('Guilherme',17,1);
+INSERT INTO mytable(Nome,Idade,AnosdeExperiencianaArea) VALUES ('Jânio',19,1);
+INSERT INTO mytable(Nome,Idade,AnosdeExperiencianaArea) VALUES ('Anderson',24,2);
+INSERT INTO mytable(Nome,Idade,AnosdeExperiencianaArea) VALUES ('Lucas',23,4);
+INSERT INTO mytable(Nome,Idade,AnosdeExperiencianaArea) VALUES ('Sara',23,2);
+INSERT INTO mytable(Nome,Idade,AnosdeExperiencianaArea) VALUES ('Angelina',23,3);
+INSERT INTO mytable(Nome,Idade,AnosdeExperiencianaArea) VALUES ('Bruno',23,4);
+INSERT INTO mytable(Nome,Idade,AnosdeExperiencianaArea) VALUES ('Pedro',27,5);
+INSERT INTO mytable(Nome,Idade,AnosdeExperiencianaArea) VALUES ('Ana Flávia',30,9);
+INSERT INTO mytable(Nome,Idade,AnosdeExperiencianaArea) VALUES ('Ana Maria',19,2);
+INSERT INTO mytable(Nome,Idade,AnosdeExperiencianaArea) VALUES ('Ana Luiza',20,1);
+INSERT INTO mytable(Nome,Idade,AnosdeExperiencianaArea) VALUES ('Matheus',20,1);
+INSERT INTO mytable(Nome,Idade,AnosdeExperiencianaArea) VALUES ('Leonardo',21,3);
+INSERT INTO mytable(Nome,Idade,AnosdeExperiencianaArea) VALUES ('Igor',25,3);
+INSERT INTO mytable(Nome,Idade,AnosdeExperiencianaArea) VALUES ('Amanda',25,4);
+INSERT INTO mytable(Nome,Idade,AnosdeExperiencianaArea) VALUES ('Jéssica',24,5);
